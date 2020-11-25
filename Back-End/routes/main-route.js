28b032b50b4204035b2ee7c01eaf39a7bc391897
Shehadeh-Al-Momani <shehadeh.almomani@gmail.com>
2020-11-25 13:58:06 +0200

    starter code

--- Back-End/routes/main-route.js
+++ Back-End/routes/main-route.js
@@ -1,11 +1,12 @@
 const mainRouter = require("express").Router();
+const db = require('../db')
 
-const adminRouter = require('./admin.route'); 
-const instructorsRouter = require('./instructors.route'); 
-const studentsRouter = require('./students.route'); 
+const adminRouter = require('./admin.route');
+const instructorsRouter = require('./instructors.route');
+const studentsRouter = require('./students.route');
 
-mainRouter.use('/admin/',adminRouter); 
-mainRouter.use('/instructors/',instructorsRouter);
-mainRouter.use('/students/',studentsRouter);
+mainRouter.use('/admin/', adminRouter);
+mainRouter.use('/instructors/', instructorsRouter);
+mainRouter.use('/students/', studentsRouter);
 
 module.exports = mainRouter;
--- Back-End/routes/students.route.js
+++ Back-End/routes/students.route.js
@@ -1,28 +1,28 @@
 const studentsRouter = require("express").Router();
 
-// View all courses
-studentsRouter.get('/courses',courses_s)
-// View a specific course's details
-studentsRouter.get('/details/:id',courseDetails_s)
-// View all categories
-studentsRouter.get('/categories',categories_s)
-// View courses by category
-studentsRouter.get('/category_courses/:id',categoryCourses_s)
-// Search for courses
-studentsRouter.get('/search/:id',search_s)
-// Filter courses
-studentsRouter.get('/filter/:id',filter_s)
-// View student's courses history
-studentsRouter.get('/history/:id',history_s)
-// Evaluation(Star rating)
-studentsRouter.add('/evaluate/:id',evaluate_s)
-// Show all rating courses
-studentsRouter.get('/rating/:id',getRating_s)
-// Enrollment in multiple course
-studentsRouter.get('/',)
-// Chat integration with instructors
-studentsRouter.get('/',)
-// Integration with Video service provider
-studentsRouter.get('/',)
+// // View all courses
+// studentsRouter.get('/courses',courses_s)
+// // View a specific course's details
+// studentsRouter.get('/details/:id',courseDetails_s)
+// // View all categories
+// studentsRouter.get('/categories',categories_s)
+// // View courses by category
+// studentsRouter.get('/category_courses/:id',categoryCourses_s)
+// // Search for courses
+// studentsRouter.get('/search/:id',search_s)
+// // Filter courses
+// studentsRouter.get('/filter/:id',filter_s)
+// // View student's courses history
+// studentsRouter.get('/history/:id',history_s)
+// // Evaluation(Star rating)
+// studentsRouter.add('/evaluate/:id',evaluate_s)
+// // Show all rating courses
+// studentsRouter.get('/rating/:id',getRating_s)
+// // Enrollment in multiple course
+// studentsRouter.get('/',)
+// // Chat integration with instructors
+// studentsRouter.get('/',)
+// // Integration with Video service provider
+// studentsRouter.get('/',)
 
 module.exports = studentsRouter;
