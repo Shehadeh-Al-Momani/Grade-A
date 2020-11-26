74f27a32958e53d7a85434d4b14ad9267bb28fe6
Shehadeh-Al-Momani <shehadeh.almomani@gmail.com>
2020-11-26 16:36:31 +0200

    create visiblityCourse func

--- Back-End/controllers/instructors_controllers/courses_controllers.js
+++ Back-End/controllers/instructors_controllers/courses_controllers.js
@@ -1,5 +1,15 @@
 const db = require('../../db');
 
-module.exports = {
-   
+const visiblityCourse = (req, res) => {
+    const query = `UPDATE courses SET isDisabled = !isDisabled WHERE  id = ?`;
+    const data = req.params.id;
+    db.query(query, data, (err, result) => {
+        if (err) throw err;
+        res.json(result);
+    })
 };
+
+module.exports = {
+    visiblityCourse,
+};
+
