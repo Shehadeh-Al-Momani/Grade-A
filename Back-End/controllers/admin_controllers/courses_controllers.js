2676bd70416779814fdeb3bf0db9a75f77909be1
Shehadeh-Al-Momani <shehadeh.almomani@gmail.com>
2020-11-26 16:23:25 +0200

    Files arrangement

--- Back-End/controllers/admin_controllers/courses_controllers.js
+++ Back-End/controllers/admin_controllers/courses_controllers.js
@@ -1,64 +1,6 @@
 const db = require('../../db');
 
-const getAllCourses = (req, res) => {
-    const query = 'SELECT * FROM courses';
-    db.query(query, (err, result) => {
-        if (err) throw err;
-        res.json(result);
-    })
-};
-
-const courseDetails = (req, res) => {
-    const query = 'SELECT * FROM courses WHERE id=?';
-    const data = req.params.id;
-    db.query(query, data, (err, result) => {
-        if (err) throw err;
-        res.json(result);
-    })
-};
-
-const getAllCategories = (req, res) => {
-    const query = 'SELECT * FROM categories';
-    db.query(query, (err, result) => {
-        if (err) throw err;
-        res.json(result);
-    })
-};
-
-const searchCourses = (req, res) => {
-    const data = req.params.id;
-    const query = `SELECT * FROM courses WHERE name LIKE'%${data}%'`;
-    db.query(query, data, (err, result) => {
-        if (err) throw err;
-        res.json(result);
-    })
-};
-
-const filterCourses = (req, res) => {
-    const query = `SELECT * FROM courses JOIN rating ON rating.course_id=courses.id AND rating.rating_value > ?`;
-    const data = req.params.id;
-    db.query(query, data, (err, result) => {
-        if (err) throw err;
-        res.json(result);
-    })
-};
-
-const historyCourses = (req, res) => {
-    const query = `SELECT * FROM courses JOIN registration ON courses.id=registration.course_id AND registration.student_id =?`;
-    const data = req.params.id;
-    db.query(query, data, (err, result) => {
-        if (err) throw err;
-        res.json(result);
-    })
-};
-
 module.exports = {
-    getAllCourses,
-    courseDetails,
-    getAllCategories,
-    searchCourses,
-    filterCourses,
-    historyCourses,
-
+    
 };
 
