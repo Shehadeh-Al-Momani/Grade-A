f5bdfe8d8a0fce6f3e8b01a920070cf1ba155a7a
Shehadeh-Al-Momani <shehadeh.almomani@gmail.com>
2020-11-26 16:37:34 +0200

    create addCourse func

--- Back-End/controllers/instructors_controllers/courses_controllers.js
+++ Back-End/controllers/instructors_controllers/courses_controllers.js
@@ -9,7 +9,18 @@ const visiblityCourse = (req, res) => {
     })
 };
 
+const addCourse = (req, res) => {
+    const {name,price,description,instructor_id,category_id} = req.body;
+    const query = `INSERT INTO courses (name,price,description,instructor_id,created_at,category_id) values (?,?,?,?,?,?)`;
+    const data = [name,price,description,instructor_id,new Date,category_id];
+    db.query(query, data, (err, result) => {
+        if (err) throw err;
+        res.json(result);
+    })
+};
+
 module.exports = {
     visiblityCourse,
+    addCourse,
 };
 
