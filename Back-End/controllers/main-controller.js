871e64dc05746196bfa39eabe0b5a4a332cf5a77
Shehadeh Al-Momani <70090805+Shehadeh-Al-Momani@users.noreply.github.com>
2020-11-26 11:40:20 +0200

    Update main-controller.js

--- Back-End/controllers/main-controller.js
+++ Back-End/controllers/main-controller.js
@@ -1,11 +1,11 @@
 const db = require('../db');
-const {categoryCourses_s, addCourse_s, evaluate_s, getRating_s} = require('./students_controllers');
+const {getCategoryCourses, addCourse, evaluate, getRating} = require('./students_controllers');
 
 module.exports = {
-    categoryCourses_s,
-    addCourse_s, 
-    evaluate_s, 
-    getRating_s,    
+    getCategoryCourses,
+    addCourse, 
+    evaluate, 
+    getRating,    
 };
 
 
