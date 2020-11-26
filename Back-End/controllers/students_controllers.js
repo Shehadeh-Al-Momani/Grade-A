83bff47eec32b56b902b2f854f28741dc354a101
Shehadeh Al-Momani <70090805+Shehadeh-Al-Momani@users.noreply.github.com>
2020-11-26 11:37:05 +0200

    Update students_controllers.js

--- Back-End/controllers/students_controllers.js
+++ Back-End/controllers/students_controllers.js
@@ -1,56 +1,47 @@
 const db = require('../db');
 
 const categoryCourses_s = (req, res) => {
- console.log("REQ Params: ", req.params.id)
- let sql = `SELECT * FROM courses WHERE category_id = ${req.params.id}`
- let query = db.query(sql, (err, result) => {
-    if (err) throw err
-    console.log("categoryCourses_s called")
-    res.json(result)
-})
+    const query = `SELECT * FROM courses WHERE category_id = ?`;
+    const data = req.params.id;
+    db.query(query, data, (err, result) => {
+        if (err) throw err
+        res.json(result)
+    })
 }
 
 const addCourse_s = (req, res) => {
-    console.log("REQ PARAMS: ", req.params.id)
-    console.log("REQ BODY: ",req.body)
-    let sql = `INSERT INTO registration (student_id, course_id) 
-    VALUES ("${req.body.student_id}", "${req.params.id}")`
-    let query = db.query(sql, (err, result) => {
+    const query = `INSERT INTO registration (student_id, course_id) VALUES (?,?)`;
+    const data = [req.body.student_id,req.params.id];
+    db.query(query,data, (err, result) => {
         if (err) throw err
-        console.log("addCourse_s called")
         res.send("addCourse_s called")
         // res.json(result)
     })
 }
 
 const evaluate_s = (req, res) => {
-    console.log("REQ PARAMS: ", req.params.id)
-    console.log("REQ BODY: ", req.body)
-    let sql = `INSERT INTO rating (student_id, course_id, rating_value) 
-    VALUES ("${req.body.student_id}", "${req.params.id}", "${req.body.rating}")`
-    let query = db.query(sql, (err, result) => {
+    const query = `INSERT INTO rating (student_id, course_id, rating_value) VALUES (?, ?, ?)`;
+    const data = [req.body.student_id,req.params.id,req.body.rating];
+    db.query(query,data, (err, result) => {
         if (err) throw err
-        console.log("evaluate_s called")
         res.send("evaluate_s called")
         // res.json(result)
     })
 }
 
 const getRating_s = (req, res) => {
-    console.log("REQ Params: ", req.params.id)
- let sql = `SELECT * FROM rating WHERE rating_value = ${req.params.id}`
- let query = db.query(sql, (err, result) => {
-    if (err) throw err
-    console.log("getRating_s called")
-    res.json(result)
-})
+    const query = `SELECT * FROM rating WHERE rating_value = ?`;
+    const data = req.params.id;
+    db.query(query,data, (err, result) => {
+        if (err) throw err
+        res.json(result)
+    })
 }
 
 module.exports = {
-    categoryCourses_s, 
-    addCourse_s, 
+    categoryCourses_s,
+    addCourse_s,
     evaluate_s,
     getRating_s
+};
 
-
-}
