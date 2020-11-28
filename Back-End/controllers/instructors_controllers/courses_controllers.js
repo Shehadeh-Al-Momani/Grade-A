a26f7d48a633ab370559fa8d3e658002ca60707d
Omar Alkhatib <omar.q.alkhatib@gmail.com>
2020-11-28 18:05:35 +0200

    Some corrections added

--- Back-End/controllers/instructors_controllers/courses_controllers.js
+++ Back-End/controllers/instructors_controllers/courses_controllers.js
@@ -1,7 +1,7 @@
 const db = require('../../db');
 
 const registeredCoursesInstructor = (req, res) => {
-    const query = `SELECT COUNT(*) FROM courses c JOIN registration r ON c.id = r.course_id AND instructor_id = ?`;
+    const query = `SELECT * FROM courses c JOIN registration r ON c.id = r.course_id AND instructor_id = ?`;
     const data = req.params.id;
     db.query(query, data, (err, result) => {
         if (err) throw err
