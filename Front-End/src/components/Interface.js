86c744b09772aad4d3c413064e066718beaae541
Alaa-Khalaila <alkhalila.alaa@gmail.com>
2020-12-01 13:13:32 +0200

    fixed some styles

--- Front-End/src/components/Interface.js
+++ Front-End/src/components/Interface.js
@@ -2,24 +2,23 @@ import React, { Component } from 'react'
 import wlc from "./pics/wlc.jpg"
 
 
-const wlcStyle={
-    backgroundImage:`url(${wlc})`,
-    height:"400px",
-    backgroundSize:"cover",
-    backgroundPosition:"center",
-}
+// const wlcStyle={
+//     backgroundImage:`url(${wlc})`,
+//     height:"400px",
+//     backgroundSize:"cover",
+//     backgroundPosition:"center",
+// }
 
 export class Interface extends Component {
     render() {
         return (
-            <div style={wlcStyle} className="wlc">
+            <div  className="wlc">
+                <img src={wlc}></img>
                 <div className="innerTxt">
                     <h1>For every student,<br></br>every classroom.<br></br>Real results.</h1>
                     <h4>Build skills with courses, online from best instructors</h4>  
                     <button>Join For Free</button> 
-                </div>
-                           
-                
+                </div>   
             </div>
         )
     }
