9cd4f730f1d2c648412c02551f83b98c782f3c2c
Alaa-Khalaila <alkhalila.alaa@gmail.com>
2020-12-03 02:18:41 +0200

    deleted comment

--- Front-End/src/components/Interface.js
+++ Front-End/src/components/Interface.js
@@ -1,14 +1,6 @@
 import React, { Component } from 'react'
 import wlc from "./pics/wlc.jpg"
 
-
-// const wlcStyle={
-//     backgroundImage:`url(${wlc})`,
-//     height:"400px",
-//     backgroundSize:"cover",
-//     backgroundPosition:"center",
-// }
-
 export class Interface extends Component {
     render() {
         return (
