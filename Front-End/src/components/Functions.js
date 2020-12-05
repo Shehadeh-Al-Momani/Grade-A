8c2303af2b7627f09e84e7071f8aa78a90ebcdaa
Alaa-Khalaila <alkhalila.alaa@gmail.com>
2020-12-05 07:59:38 +0200

    added on click event on function component

--- Front-End/src/components/Functions.js
+++ Front-End/src/components/Functions.js
@@ -1,12 +1,15 @@
 import React from 'react'
+import { useHistory } from "react-router-dom";
 
 const Functions = () => {
+    const history = useHistory();
+
     return (
         <div className="container">
         <div className="functions">
-            <button>Add Category</button>
+            <button onClick={()=>history.push('/')}>Add Category</button>
             <button>Students Details</button>
-            <button>Disable User</button>
+            <button onClick={()=>history.push('/disable')}>Disable User</button>
             <button>Instructors Details</button>                    
         </div>
     </div>
