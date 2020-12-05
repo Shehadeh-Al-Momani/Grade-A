9fb21964cae165d6cdeca88143827111eb361bca
Alaa-Khalaila <alkhalila.alaa@gmail.com>
2020-12-05 06:52:41 +0200

    Fixed addCategory api , almost finished addCategory component

--- Front-End/src/components/AddCategory.js
+++ Front-End/src/components/AddCategory.js
@@ -1,12 +1,26 @@
-import React from 'react'
+import React,{useState} from 'react'
+import axios from 'axios'
 
 const AddCategory = () => {
+
+    const [name,setname]=useState('')
+
+    const nameHandler =(e)=>{
+        setname(e.target.value)
+        console.log(name)
+    }
+    const addCat = ()=>{
+        axios.post('http://localhost:5000/admin/category',{name})
+        console.log(name,1)
+
+    }
+
     return (
         <div className="AddCat">
             <h2>Add Category</h2>
                 <h4>Name of the new Category</h4>
-            <input></input>
-            <button>Add</button>
+            <input onChange={nameHandler}></input>
+            <button onClick={addCat}>Add</button>
         </div>
     )
 }
