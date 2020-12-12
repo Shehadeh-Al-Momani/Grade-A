c9ad491ea74b2b79b3d8404eb20519412132a146
Shehadeh-Al-Momani <shehadeh.almomani@gmail.com>
2020-12-12 15:14:24 +0200

    move component from students.js to app.js

--- Front-End/src/App.js
+++ Front-End/src/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router } from 'react-router-dom';
-import Students from './Students';
+
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import Signup from './components/Signup';
+import LogIn from './components/LogIn';
+import Course from './components/Course ';
+import CategoryCourses from './components/Category_courses';
+import PageNotFound from './components/PageNotFound';
 
 const App = () => {
 	return (
 		<Router>
-			<Students />
+			<Navbar />
+			<Switch>
+				<Route exact path='/' render={(props) => <Home  {...props} />} />
+				<Route path='/join/login' render={(props) => <LogIn  {...props} />} />
+				<Route path='/join/signup' render={(props) => <Signup  {...props} />} />
+				<Route path='/categories/:id' render={(props) => <CategoryCourses  {...props} />} />
+				<Route path='/courses/:id' render={(props) => <Course  {...props} />} />
+				<Route render={(props) => <PageNotFound  {...props} />} />
+			</Switch>
 		</Router>
 	);
 };
