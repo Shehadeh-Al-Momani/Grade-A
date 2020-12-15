a8e0d522d6571e17efef8e9f57f7b4abe7a2bbb5
Shehadeh-Al-Momani <shehadeh.almomani@gmail.com>
2020-12-15 20:45:29 +0200

    create function in back end to get all register coursees

--- Back-End/routes/students.route.js
+++ Back-End/routes/students.route.js
@@ -1,25 +1,25 @@
 const studentsRouter = require("express").Router();
-const { getAllCourses,courseDetails,getAllCategories,searchCourses,
-    filterCourses, historyCourses,enrollmentCourse,getCategoryCourses, evaluate,getRating,
-    getAllInstructors, getAllCoursesByInstructor, getInstructor} = require('../controllers/main-controller');
+const { getAllCourses, courseDetails, getAllCategories, searchCourses,
+    filterCourses, historyCourses, enrollmentCourse, getCategoryCourses, evaluate, getRating,
+    getAllInstructors, getAllCoursesByInstructor, getInstructor, registerCourse } = require('../controllers/main-controller');
 
 // View all courses
-studentsRouter.get('/courses',getAllCourses);
+studentsRouter.get('/courses', getAllCourses);
 
 // View a specific course's details
-studentsRouter.get('/details/:id',courseDetails);
+studentsRouter.get('/details/:id', courseDetails);
 
 // View all categories
-studentsRouter.get('/categories',getAllCategories);
+studentsRouter.get('/categories', getAllCategories);
 
 // Search for courses
-studentsRouter.get('/search/:id',searchCourses);
+studentsRouter.get('/search/:id', searchCourses);
 
 // Filter courses
-studentsRouter.get('/filter/:id',filterCourses);
+studentsRouter.get('/filter/:id', filterCourses);
 
 // View student's courses history
-studentsRouter.get('/history/:id',historyCourses);
+studentsRouter.get('/history/:id', historyCourses);
 
 // Enrollment in multiple course
 studentsRouter.post('/add_course:id/:i', enrollmentCourse); //send request with "course_id" in id params and "student_id" i params
@@ -31,16 +31,19 @@ studentsRouter.get('/category_courses/:id', getCategoryCourses);
 studentsRouter.post('/evaluate/:c/:s/:r', evaluate); //send "course_id" in :c params, "student_id" in :s params, and "rating" in :r params
 
 // Show all rating courses
-studentsRouter.get('/rating/:id',getRating); // id = "rating_value"
+studentsRouter.get('/rating/:id', getRating); // id = "rating_value"
 
 // View all instructors
-studentsRouter.get('/instructors/:id',getAllInstructors); // in our project id=2 for instructors
+studentsRouter.get('/instructors/:id', getAllInstructors); // in our project id=2 for instructors
 
 // View all courses by instructors
-studentsRouter.get('/instructor_courses/:id',getAllCoursesByInstructor); // id=instructor_id 
+studentsRouter.get('/instructor_courses/:id', getAllCoursesByInstructor); // id=instructor_id 
 
 // View one instructor
-studentsRouter.get('/instructor_datails/:id',getInstructor); // id=instructor_id 
+studentsRouter.get('/instructor_datails/:id', getInstructor); // id=instructor_id 
+
+// View all registered courses 
+studentsRouter.get('/registerCourse/:id', registerCourse); // id=students_id 
 
 // Chat integration with instructors
 
