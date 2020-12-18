97bab03c71bb865f6d85080fc8e32f572756522e
Shehadeh-Al-Momani <shehadeh.almomani@gmail.com>
2020-12-18 13:34:20 +0200

    create button to enrolment

--- Front-End/src/components/Course.js
+++ Front-End/src/components/Course.js
@@ -25,26 +25,35 @@ const Course = ({ match: { params: { id }, }, }) => {
 
 	useEffect(() => {
 		axios.get(`http://localhost:5000/students/history/${stuId}`)
-			.then((response) => {
-				setEnrollmentCourses(response.data);
+			.then((res) => {
+				console.log('res.data :', res.data)
+				setEnrollmentCourses(res.data);
 			})
 			.catch((err) => { console.log('err :', err) });
 	}, [stuId]);
 
-	const arr = enrollmentCourses.filter((e) => { return e.id === course.id })
-	console.log('arr :', arr)
+	const enrollment = enrollmentCourses.filter((e) => { return e.course_id === course.courseId }).length > 0;
 	return (
-		<div className='course'>
-			<div>
-				<img src={`${course.img_course}`} alt={`${course.name}`} />
-			</div>
-			<div className='course-details'>
-				<h2> {' '} <span>Course Name : </span> {course.course}{' '} </h2>
-				<h2> {' '} <span>Category : </span> {course.category}{' '} </h2>
-				<h3> {' '} <span>Course Description : </span> {course.description} </h3>
-				<h3> {' '} <span>Instructor : </span> {course.instructor} </h3>
-				<h3> {' '} <span></span> {course.rating} </h3>
-				<h3> {' '} <span>Price: </span> {course.price + ' $'}{' '} </h3>
+		<div>
+			<div className='course'>
+				<div>
+					<img src={`${course.img_course}`} alt={`${course.name}`} />
+				</div>
+				<div className='course-details'>
+					<h2> {' '} <span>Course Name : </span> {course.course}{' '} </h2>
+					<h2> {' '} <span>Category : </span> {course.category}{' '} </h2>
+					<h3> {' '} <span>Course Description : </span> {course.description} </h3>
+					<h3> {' '} {course.instructor} </h3>
+					<h3> {' '} {course.rating} </h3>
+					<h3> {' '} {course.price + ' $'}{' '} </h3>
+					<div className='innerTxt'>
+						{
+							(!enrollment) ? (
+								<button> Enrollment</button>
+							) : null
+						}
+					</div>
+				</div>
 			</div>
 		</div>
 	);
